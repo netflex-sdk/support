03aed94468949a7ea823f6e8cc1a61c5af0c4ce7
Thomas Alrek <thomas@alrek.no>
2021-02-01 11:03:51 +0100

    Add HtmlString class with json serialization support

---
+++ src/HtmlString.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Netflex\Support;
+
+use Illuminate\Support\HtmlString as BaseHtmlString;
+
+use JsonSerializable;
+use Illuminate\Contracts\Support\Jsonable;
+
+class HtmlString extends BaseHtmlString implements JsonSerializable, Jsonable
+{
+    public function jsonSerialize()
+    {
+        return $this->__toString();
+    }
+
+    public function toJson($options = 0)
+    {
+        return json_encode($this->jsonSerialize(), $options);
+    }
+}
