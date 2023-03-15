37a8c1f92449f4b5c89033f36b04fc9d3fac8b93
Thomas <thomas@alrek.no>
2023-03-15 09:19:10 +0000

    prepare release

--- ItemCollection.php
+++ ItemCollection.php
@@ -61,6 +61,97 @@ abstract class ItemCollection extends BaseCollection implements JsonSerializable
   }
 
   /**
+   * Get and remove the last N items from the collection.
+   *
+   * @param int $count
+   * @return mixed
+   */
+  public function pop($count = 1)
+  {
+    $modifies = true;
+
+    if ($this->isEmpty()) {
+      $modifies = false;
+    }
+
+    $result = parent::pop($count);
+
+    if ($modifies) {
+      $this->performHook('modified');
+    }
+
+    return $result;
+  }
+
+  /**
+   * Push an item onto the beginning of the collection.
+   *
+   * @param mixed $value
+   * @param mixed $key
+   * @return $this
+   */
+  public function prepend($value, $key = null)
+  {
+    return parent::prepend($value, $key);
+    $this->performHook('modified');
+    return $this;
+  }
+
+  /**
+   * Push one or more items onto the end of the collection.
+   *
+   * @param mixed $values
+   * @return $this
+   */
+  public function push(...$values)
+  {
+    parent::push(...$values);
+    $this->performHook('modified');
+    return $this;
+  }
+
+  /**
+   * Splice a portion of the underlying collection array.
+   *
+   * @param int $offset
+   * @param int|null $length
+   * @param mixed $replacement
+   * @return static
+   */
+  public function splice($offset, $length = null, $replacement = [])
+  {
+    parent::splice($offset, $length, $replacement);
+    $this->performHook('modified');
+    return $this;
+  }
+
+  /**
+   * Transform each item in the collection using a callback.
+   *
+   * @param callable $callback
+   * @return $this
+   */
+  public function transform(callable $callback)
+  {
+    parent::transform($callback);
+    $this->performHook('modified');
+    return $this;
+  }
+
+  /**
+   * Add an item to the collection.
+   *
+   * @param mixed $item
+   * @return $this
+   */
+  public function add($item)
+  {
+    parent::add($item);
+    $this->performHook('modified');
+    return $this;
+  }
+
+  /**
    * Set the item at a given offset.
    *
    * @param string|int $offset
@@ -100,14 +191,107 @@ abstract class ItemCollection extends BaseCollection implements JsonSerializable
   }
 
   /**
+   * Get the items in the collection that are not present in the given items.
+   *
+   * @param  mixed  $items
+   * @return BaseCollection
+   */
+  public function diff($items)
+  {
+    return $this->toBase()->diff($items);
+  }
+
+  /**
+   * Get the items in the collection that are not present in the given items, using the callback.
+   *
+   * @param  mixed  $items
+   * @param  callable  $callback
+   * @return BaseCollection
+   */
+  public function diffUsing($items, callable $callback)
+  {
+    return $this->toBase()->diffUsing($items, $callback);
+  }
+
+  /**
+   * Run a filter over each of the items.
+   *
+   * @param  callable|null  $callback
+   * @return BaseCollection
+   */
+  public function filter(callable $callback = null)
+  {
+    return $this->toBase()->filter($callback);
+  }
+
+  /**
+   * Flip the items in the collection.
+   *
+   * @return BaseCollection
+   */
+  public function flip()
+  {
+    return $this->toBase()->flip();
+  }
+
+  /**
+   * Intersect the collection with the given items.
+   *
+   * @param  mixed  $items
+   * @return BaseCollection
+   */
+  public function intersect($items)
+  {
+    return $this->toBase()->intersect($items);
+  }
+
+  /**
+   * Get the values of a given key.
+   *
+   * @param  string|array|int|null  $value
+   * @param  string|null  $key
+   * @return BaseCollection
+   */
+  public function pluck($value, $key = null)
+  {
+    return $this->toBase()->pluck($value, $key);
+  }
+
+  /**
    * Run a map over each of the items.
    *
    * @param callable $callback
-   * @return Illuminate\Support\Collection
+   * @return BaseCollection
    */
   public function map(callable $callback)
   {
-    return new BaseCollection(parent::map($callback));
+    return $this->toBase()->map($callback);
+  }
+
+  /**
+   * Run a dictionary map over the items.
+   *
+   * The callback should return an associative array with a single key/value pair.
+   *
+   * @param  callable  $callback
+   * @return BaseCollection
+   */
+  public function mapToDictionary(callable $callback)
+  {
+    return new BaseCollection(parent::mapToDictionary($callback));
+  }
+
+  /**
+   * Run an associative map over each of the items.
+   *
+   * The callback should return an associative array with a single key/value pair.
+   *
+   * @param  callable  $callback
+   * @return BaseCollection
+   */
+  public function mapWithKeys(callable $callback)
+  {
+    return new BaseCollection(parent::mapWithKeys($callback));
   }
 
   /**
